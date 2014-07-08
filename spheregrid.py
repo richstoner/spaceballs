f925940a8d84b309e5756d115d050b6270ea180f
richstoner <stonerri@gmail.com>
2014-07-08 13:03:40 -0400

    added extern spheres

--- spheregrid.py
+++ spheregrid.py
@@ -34,6 +34,12 @@ class SphereGrid(object):
 
 
     def getSpheresWithinExtent(self, bb, rotate=False):
+        ''' Get all sphers within the outer bounding box
+
+        :param bb:
+        :param rotate:
+        :return:
+        '''
 
         sphere_list = []
 
@@ -66,8 +72,64 @@ class SphereGrid(object):
 
 
 
+    def getSpheresOnlyWithinExtent(self, bb, rotate=False):
+        '''  returns only the spheres within the outer bounding box, but not the inner
+
+        :param bb:
+        :param rotate:
+        :return:
+        '''
+
+        sphere_list = []
+
+        points = self.pointList
+
+        for point in points:
+
+            res_inner = ''
+            res_outer = ''
+
+            for plane in bb.innerPlanes:
+
+                if pointAbovePlane(plane, point):
+                    res_inner += '1'
+                else:
+                    res_inner += '0'
+
+            for plane in bb.outerPlanes:
+
+                if pointAbovePlane(plane, point):
+                    res_outer += '1'
+                else:
+                    res_outer += '0'
+
+
+            if (int(res_outer, 2) == 0) and (int(res_inner, 2) > 0):
+
+                if rotate:
+                    # flip rotation on point
+                    new_point = bb.rotatePoint(point, - bb.theta, - bb.phi)
+                    sphere_list.append(new_point)
+
+                else:
+
+                    sphere_list.append(point)
+
+
+        return np.asarray(sphere_list)
+
+
+
+
 
     def getSpheresWithinBoundingBox(self, bb, rotate=False):
+        ''' get spheres only within inner bounding box
+
+        :param bb:
+        :param rotate:
+        :return:
+        '''
+
 
         sphere_list = []
 
